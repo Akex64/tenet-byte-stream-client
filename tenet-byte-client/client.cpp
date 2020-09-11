a3cb5f787586cf5836b65214c9422c0c21766335
Akex64 <61739633+Akex64@users.noreply.github.com>
2020-09-11 22:39:07 +0200

    Update client.cpp

--- tenet-byte-client/client.cpp
+++ tenet-byte-client/client.cpp
@@ -12,16 +12,16 @@ void client::initialize(std::string ip, int port) {
 
 	SOCKADDR_IN addr;
 	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr(ip);
+	addr.sin_addr.s_addr = inet_addr(ip.c_str());
 	addr.sin_port = htons(port);
 	addr.sin_family = AF_INET;
 
-	connection = socket(AF_INET, SOCK_STREAM, NULL);
+	this->connection = socket(AF_INET, SOCK_STREAM, NULL);
 
-	if (connect(connection, (SOCKADDR*)&addr, sizeofaddr) != 0)
+	if (connect(this->connection, (SOCKADDR*)&addr, sizeofaddr) != 0)
 	{
 		MessageBoxA(NULL, "Failed to connect server.", "Error", MB_OK | MB_ICONERROR);
-		return;
+		exit(0);
 	}
 	std::cout << "Connected" << std::endl;
 }
